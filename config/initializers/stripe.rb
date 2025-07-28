bf974d04a444fee57c719362aafe96df36542674
goodbye-isaac <goodbyeisaac1727@gmail.com>
2025-07-28 23:07:08 +0900

    fix: Stripe Apiキーが読み込まれない問題を修正

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,3 @@
-Stripe.api_key= Rails.application.credentials.dig(:stripe, :sekret_key)
-Stripe.api_version = '2025-06-30.basil'
+require "stripe"
+Stripe.api_key= Rails.application.credentials.dig(:stripe, :secret_key)
+Stripe.api_version = "2025-06-30.basil"
